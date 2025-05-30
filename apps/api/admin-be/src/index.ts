60120309885822f0fbfba69280d4c41e70850c8b
MistaHolmes <abhasbehera560@gmail.com>
2025-05-30 09:06:23 +0530

    Cors

--- apps/api/admin-be/src/index.ts
+++ apps/api/admin-be/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import superAdminRoutes from './routes/superAdminRoutes';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 
 
@@ -9,6 +10,12 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'http://localhost:3003'], 
+    credentials: true,
+  })
+);
 
 app.use('/api/super-admin', superAdminRoutes);
 
