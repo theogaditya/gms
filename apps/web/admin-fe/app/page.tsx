bb081fcf18ddb008a701ae6be870cb454db9f112
MistaHolmes <abhasbehera560@gmail.com>
2025-06-04 10:32:02 +0530

    minor fix

--- apps/web/admin-fe/app/page.tsx
+++ apps/web/admin-fe/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
 import AdminSignIn from "./auth/page";
 
 export default function Home() {
   return (
     <div>
       <AdminSignIn/>
-      
     </div>
   );
 }
