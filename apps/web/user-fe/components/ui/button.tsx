932045f16f477008dddb1bb7a0cc59e8dcc871b2
theogaditya <adityahota99@gmail.com>
2025-06-14 15:44:14 +0530

    na

--- apps/web/user-fe/components/ui/button.tsx
+++ apps/web/user-fe/components/ui/button.tsx
@@ -43,14 +43,12 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    )
-  },
+    // If asChild, we must cast props to any to satisfy Slot typing
+    const CompProps = asChild
+      ? ({ ref, className: cn(buttonVariants({ variant, size, className })), ...props } as any)
+      : { ref, className: cn(buttonVariants({ variant, size, className })), ...props }
+    return <Comp {...CompProps} />
+  }
 )
 Button.displayName = "Button"
 
